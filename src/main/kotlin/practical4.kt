28be1fcd708310ce7236c0d6d998a572058b214c
roshnipatell <patelroshni9045@gmail.com>
2022-08-10 14:32:29 +0530

    complete pr4

---
+++ src/main/kotlin/practical4.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    println("Enter The Number x:")
+    var x= readLine()!!.toInt()
+
+
+   print (if (x%2==0)
+
+        ("$x is Even")
+
+        else
+                ("$x is odd"))
+
+
+
+}
